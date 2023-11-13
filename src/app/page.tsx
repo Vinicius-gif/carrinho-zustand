a5c7d732bb0a9fb321b812834f09527a32c3d4ed
Vinicius <vfpv@hotmail.com>
2023-11-12 22:05:51 -0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import Carrinho from '@/components/Carrinho';
 import Products from '@/components/Products';
 import React from 'react';
 
@@ -9,7 +8,6 @@ export default function Home() {
   return (
     <main>
       <Products/>
-      <Carrinho/>
     </main>
   );
 }
