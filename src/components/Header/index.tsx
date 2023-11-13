daffa7e5e70982808e905be9bdc08746f01ba05c
Vinicius <vfpv@hotmail.com>
2023-11-12 22:06:54 -0300

    add link

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,12 +4,16 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import CartButton from './CartButton';
+import Link from 'next/link';
 
 const Header = () => {
 
   return (
     <header className="bg-sky-500 h-16 w-full flex items-center fixed z-10">
       <div className="flex items-center justify-around w-4/5">
+        <Link href="/">
+          <h2 className="font-bold text-4xl text-white">Logo</h2>
+        </Link>
         <SearchBar/>
         <CartButton/>
       </div>
--- src/components/Products/ProductCard/index.tsx
+++ src/components/Products/ProductCard/index.tsx
@@ -4,6 +4,7 @@ import formatCurrency from '@/utils/formatCurrency';
 import React from 'react';
 import { BsFillCartPlusFill } from 'react-icons/bs';
 import { Product } from '@/types/product';
+import Link from 'next/link';
 
 
 const ProductCard = ({id, title, price, thumbnail }: Product) => {
@@ -14,24 +15,25 @@ const ProductCard = ({id, title, price, thumbnail }: Product) => {
     <section 
       className="group w-full max-w-xs bg-white flex flex-col cursor-pointer my-0 mx-auto relative
       hover:shadow-lg hover:flex">
-      <img
-        src={thumbnail.replace(/\w\.jpg/gi, 'W.jpg')}
-        alt="product"
-        className="w-full"
-      />
-
-      <div className="p-5 border-t border-gray-400">
-        <h2 className="text-3xl font-normal block mb-3">
-          {formatCurrency({value: price})}
-        </h2>
-        <h2 className="text-sm text-gray-900 leading-6 font-medium">{title}</h2>
-      </div>
+      <Link href={`/product/${id}`}>
+        <img
+          src={thumbnail.replace(/\w\.jpg/gi, 'W.jpg')}
+          alt="product"
+          className="w-full"
+        />
+        <div className="p-5 border-t border-gray-400">
+          <h2 className="text-3xl font-normal block mb-3">
+            {formatCurrency({value: price})}
+          </h2>
+          <h2 className="text-sm text-gray-900 leading-6 font-medium">{title}</h2>
+        </div>
+      </Link>
 
       <button
         type="button"
         onClick={() => addToCart(id)}
         className="absolute top-0 right-0 w-11 h-11 my-3 mx-4 text-blue-600 bg-white items-center
-        justify-center border-none rounded-l-3xl text-2xl cursor-pointer hidden group-hover:flex"
+        justify-center border-none rounded-l-3xl text-2xl cursor-pointer hidden group-hover:flex "
       >
         <BsFillCartPlusFill />
       </button>
